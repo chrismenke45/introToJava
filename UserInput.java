6a3f5f668a3a064b081f4d4bf8008367e5dac289
chrismenke45 <chrismenke45@gmail.com>
2023-05-29 13:35:34 -0700

    fully functional

--- UserInput.java
+++ UserInput.java
@@ -1,31 +1,52 @@
 import java.util.Scanner;
 
 public class UserInput {
+    public static int indexOfStringArray(String[] array, String key) {
+        int returnvalue = -1;
+        for (int i = 0; i < array.length; ++i) {
+            if (key.equals(array[i])) {
+                returnvalue = i;
+                break;
+            }
+        }
+        return returnvalue;
+    }
+
     public static void main(String args[]) {
         Boolean properInput = false;
         String choice = "";
+        String[] possibleMoves = {"rock", "paper", "scissors"};
+        int choiceIndex = -1;
         Scanner scanner = new Scanner(System.in);
+
         while (!properInput) {
             System.out.println("Rock, Paper or Scissors?");
             choice = scanner.nextLine().toLowerCase();
-            if (choice.equals("rock")) {
-                properInput = true;
-            } else if (choice.equals("paper")) {
-                properInput = true;
-            } else if (choice.equals("scissors")) {
+            choiceIndex = indexOfStringArray(possibleMoves, choice);
+            if (indexOfStringArray(possibleMoves, choice) != -1) {
                 properInput = true;
             }
         }
         scanner.close();
-        String[] computerMoves = {"rock", "paper", "scissors"};
+        
         int randomMove = (int) Math.floor(Math.random() * 3);
-        String computerMove = computerMoves[randomMove];
+        String computerMove = possibleMoves[randomMove];
+        int computerIndex = indexOfStringArray(possibleMoves, computerMove);
+
         if (computerMove.equals(choice)) {
-            System.out.println("Tie! You both chose %s");
+            System.out.println("Tie! You both chose " + choice);
+        } else if (Math.abs(computerIndex - choiceIndex) == 1) {
+            if(computerIndex < choiceIndex) {
+                System.out.println("You won! You chose: " + choice + " and the computer chose: " + computerMove);
+            } else {
+                System.out.println("You lost! You chose: " + choice + " and the computer chose: " + computerMove);
+            }
+        } else {
+            if (choiceIndex == 0) {
+                System.out.println("You won! You chose: " + choice + " and the computer chose: " + computerMove);
+            } else {
+                System.out.println("You lost! You chose: " + choice + " and the computer chose: " + computerMove);
+            }
         }
-
-
-        System.out.println(choice);
-        System.out.println(computerMove);
     }
 }
